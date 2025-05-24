5a90cd6fd28f40523cd781e9d8a2d5a0def340d0
korn-dev <kasikorntripforwork@gmail.com>
2025-05-24 22:22:36 +0700

    Set up ALL DONE!

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/custom-ui/toggle-theme-button";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="p-4">
+      <ModeToggle />
+      <Button>testad</Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default page;
